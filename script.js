f5de5c4927bb3116069e7c69e60400a64ee6a0f1
Humoyunbek-Tursunniyazov <tursunniyazovhumoyunbek2007@gmail.com>
2023-02-08 11:03:10 +0500

    main

--- script.js
+++ script.js
@@ -3,4 +3,46 @@ window.addEventListener("DOMContentLoaded", function () {
     const header = document.querySelector("header");
     header.classList.toggle("sticky", window.scrollY > 10);
   });
+
+  const menuBtn = document.querySelector(".menuBtn");
+  const navigation = document.querySelector(".navigation");
+  const navigationItem = document.querySelectorAll(".navigation a ");
+
+  menuBtn.addEventListener("click", () => {
+    menuBtn.classList.toggle("active");
+    navigation.classList.toggle("active");
+  });
+
+  navigationItem.forEach((navItem) => {
+    navItem.addEventListener("click", () => {
+      menuBtn.classList.remove("active");
+      navigation.classList.remove("active");
+    });
+  });
+
+  const scrollBtn = document.querySelector(".scrollToTop-btn");
+  window.addEventListener("scroll", () => {
+    scrollBtn.classList.toggle("active", window.scrollY > 500);
+  });
+  scrollBtn.addEventListener("click", () => {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
+
+  window.addEventListener("scroll", () => {
+    let reveals = document.querySelectorAll(".reveal");
+
+    for (let i = 0; reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let revealTop = reveals[i].getBoundingClientRect().top;
+      let revealPoint = 50;
+
+      if (revealTop < windowHeight - revealPoint) {
+        reveals[i].classList.add("active");
+      } 
+      // else {
+      //   reveals[i].classList.remove("active");
+      // }
+    }
+  });
 });
